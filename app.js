99d56aded12697bb900e4edf452aaaa2e5cf0165
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-08-24 17:37:29 +0100

    Add basic new message functionality

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
+
+// set up live reloading
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+        liveReloadServer.refresh('/');
+    }, 10);
+});
+
+app.use(connectLiveReload());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +32,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+    {
+        text: 'Hi there!',
+        user: 'Amando',
+        added: new Date(),
+    },
+    {
+        text: 'Hello World!',
+        user: 'Charles',
+        added: new Date(),
+    },
+];
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => res.render('index', { messages: messages }));
+
+/* new message */
+router.get('/new', (req, res, next) => res.render('form'));
+
+router.post('/new', (req, res, next) => {
+    messages.push({
+        text: req.body.message,
+        user: req.body.name,
+        added: new Date(),
+    });
+    res.redirect('/');
 });
 
 module.exports = router;
